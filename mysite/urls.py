82af901e8012c0fa3d68de129e45fb24bc741cf0
Ivan Bochenko <bochenkoivan@gmail.com>
2017-12-21 23:49:32 +0200

    make some tweaks

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^accounts/', include('registration.backends.hmac.urls')),
     path(r'^accounts/login/$', views.login, name='login'),
     path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     path(r'', include('blog.urls')),
