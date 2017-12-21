2b62689a2d3ba746c44e06511f25727dfdf46d61
Ivan Bochenko <bochenkoivan@gmail.com>
2017-12-21 21:58:47 +0200

    make some tweaks

--- blog/views.py
+++ blog/views.py
@@ -10,26 +10,26 @@ from django.db.models import Q
 # Create your views here.
 
 
-class BlogSearchListView(BlogListView):
-    """
-    Display a Blog List page filtered by the search query.
-    """
-    paginate_by = 10
-
-    def get_queryset(self):
-        result = super(BlogSearchListView, self).get_queryset()
-
-        query = self.request.GET.get('q')
-        if query:
-            query_list = query.split()
-            result = result.filter(
-                reduce(operator.and_,
-                       (Q(title__icontains=q) for q in query_list)) |
-                reduce(operator.and_,
-                       (Q(content__icontains=q) for q in query_list))
-            )
-
-        return result
+# class BlogSearchListView(BlogListView):
+#     """
+#     Display a Blog List page filtered by the search query.
+#     """
+#     paginate_by = 10
+#
+#     def get_queryset(self):
+#         result = super(BlogSearchListView, self).get_queryset()
+#
+#         query = self.request.GET.get('q')
+#         if query:
+#             query_list = query.split()
+#             result = result.filter(
+#                 reduce(operator.and_,
+#                        (Q(title__icontains=q) for q in query_list)) |
+#                 reduce(operator.and_,
+#                        (Q(content__icontains=q) for q in query_list))
+#             )
+#
+#         return result
 
 
 def post_list(request):
